786cfe8337eb3c9a71d143f967c1c1079deac695
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-08-06 09:38:57 -0500

    React Routes set up!
    Made components for About, Contact, Designs, Home and Projects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Designs from './components/Designs';
+import Projects from './components/Projects'
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/designs" component={Designs} />
+        <Route path="/projects" component={Projects} />
+      </Switch>
+    </Router>
+
+
   );
 }
 
