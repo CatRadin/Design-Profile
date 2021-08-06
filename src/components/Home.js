0f7c62ef8698db0445b1e7f32dc1e08657a34808
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-08-06 14:05:42 -0500

    Made the HTMl and CSS of Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,50 @@
+import '../css/Home.css'
+import { Link } from "react-router-dom";
+import sparkle from '../assets/sparkle.gif'
+import gardener from '../assets/gardener.jpg'
+import gamer from '../assets/switch.jpg'
+
+
+const images = [gardener, gamer]
+
 export default function Home() {
     return (
-        <>Hello!</>
+        <div className='home'>
+            <img className='sparkle' src={sparkle} alt='sparkle gif' />
+
+            <div className='middle-section'>
+                <div className='links'>
+                    <ul>
+                        <il>
+                            {/* <Link to="/">Home</Link> */}
+                        </il>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/designs">Designs</Link>
+                        </li>
+                        <li><Link to="/projects">Projects</Link></li>
+                    </ul>
+
+                </div>
+                <p><span className='name'>Catherine </span>
+                    is a UX <span className='pink-accent'>/</span> UI Designer <span className='pink-accent'>+</span> <br />
+                    Full Stack Web Developer <span className='pink-accent'>&</span> <br />
+                    canging text here<span className='pink-accent'>.</span></p>
+            </div>
+            <div className='image-container'>
+                <img className='images' src={images[0]} alt="intrest pictures" />
+            </div>
+            <p className='img-count'>1 / 4</p>
+            <div className='bottom-box'>
+                <div className='center-color-box'>
+                </div>
+            </div>
+        </div>
 
     );
 }
